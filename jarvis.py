b0495b77f138ccc6142c332a261d1b1873332e01
Tsvetkov Maxim <pushiniiist@gmail.com>
2024-04-24 21:28:55 +0200

    add typings

--- jarvis.py
+++ jarvis.py
@@ -2,19 +2,19 @@ import enum
 
 
 class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, x: float, y: float) -> None:
+        self.x: float = x
+        self.y: float = y
 
 
 class orientation_type(enum.Enum):
-    collinear = 0
-    clockwise = 1
-    counterclockwise = 2
+    collinear: int = 0
+    clockwise: int = 1
+    counterclockwise: int = 2
 
 
-def Left_index(points):
-    minn = 0
+def Left_index(points: list[Point]) -> float:
+    minn: float = 0
     for i in range(1, len(points)):
         if points[i].x < points[minn].x:
             minn = i
@@ -24,8 +24,8 @@ def Left_index(points):
     return minn
 
 
-def orientation(p, q, r):
-    val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y)
+def orientation(p: Point, q: Point, r: Point) -> int:
+    val: float = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y)
 
     if val == 0:
         return orientation_type.collinear.value
@@ -35,22 +35,22 @@ def orientation(p, q, r):
         return orientation_type.counterclockwise.value
 
 
-def convexHull(points, n):
+def convexHull(points: list[Point], n: int) -> None:
 
     if n < 3:
         return
 
-    left = Left_index(points)
+    left: float = Left_index(points)
 
-    hull = []
+    hull: list[float] = []
 
-    p = left
-    q = 0
+    p: float = left
+    q: float = 0
     while True:
 
         hull.append(p)
 
-        q = (p + 1) % n
+        q: float = (p + 1) % n
 
         for i in range(n):
             if orientation(points[p], points[i], points[q]) == 2:
@@ -65,7 +65,7 @@ def convexHull(points, n):
         print(points[each].x, points[each].y)
 
 
-points = []
+points: list[Point] = []
 points.append(Point(0, 3))
 points.append(Point(2, 2))
 points.append(Point(1, 1))
@@ -75,5 +75,3 @@ points.append(Point(0, 0))
 points.append(Point(3, 3))
 
 convexHull(points, len(points))
-
-# complexity: O(n * h), n = all points, h = output points
