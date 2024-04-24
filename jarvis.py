a4a6b6317a497947b13146991bf9ec4fdb0d02a6
Tsvetkov Maxim <pushiniiist@gmail.com>
2024-04-24 17:13:58 +0200

    linting fix

--- jarvis.py
+++ jarvis.py
@@ -2,65 +2,68 @@ import enum
 
 
 class Point:
-	def __init__(self, x, y):
-		self.x = x
-		self.y = y
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 
 class orientation_type(enum.Enum):
-	collinear = 0
-	clockwise = 1
-	counterclockwise = 2
+    collinear = 0
+    clockwise = 1
+    counterclockwise = 2
+
 
 def Left_index(points):
-	minn = 0
-	for i in range(1,len(points)):
-		if points[i].x < points[minn].x:
-			minn = i
-		elif points[i].x == points[minn].x:
-			if points[i].y > points[minn].y:
-				minn = i
-	return minn
+    minn = 0
+    for i in range(1, len(points)):
+        if points[i].x < points[minn].x:
+            minn = i
+        elif points[i].x == points[minn].x:
+            if points[i].y > points[minn].y:
+                minn = i
+    return minn
+
 
 def orientation(p, q, r):
-	val = (q.y - p.y) * (r.x - q.x) - \
-		(q.x - p.x) * (r.y - q.y)
+    val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y)
 
-	if val == 0:
-		return orientation_type.collinear.value
-	elif val > 0:
-		return orientation_type.clockwise.value
-	else:
-		return orientation_type.counterclockwise.value
+    if val == 0:
+        return orientation_type.collinear.value
+    elif val > 0:
+        return orientation_type.clockwise.value
+    else:
+        return orientation_type.counterclockwise.value
+
 
 def convexHull(points, n):
-	
-	if n < 3:
-		return
 
-	l = Left_index(points)
+    if n < 3:
+        return
 
-	hull = []
-	
-	p = l
-	q = 0
-	while(True):
-		
-		hull.append(p)
+    left = Left_index(points)
 
-		q = (p + 1) % n
+    hull = []
 
-		for i in range(n):
-			if(orientation(points[p], 
-						points[i], points[q]) == 2):
-				q = i
+    p = left
+    q = 0
+    while True:
 
-		p = q
+        hull.append(p)
 
-		if(p == l):
-			break
+        q = (p + 1) % n
 
-	for each in hull:
-		print(points[each].x, points[each].y)
+        for i in range(n):
+            if orientation(points[p], points[i], points[q]) == 2:
+                q = i
+
+        p = q
+
+        if p == left:
+            break
+
+    for each in hull:
+        print(points[each].x, points[each].y)
+
 
 points = []
 points.append(Point(0, 3))
