bc90a0d0121d055a41681af642d0a47dec48a14f
Maxim Tsvetkov <pushiniiist@gmail.com>
2024-05-14 21:39:16 +0200

    converter from adjacency matrix to adjacency list

---
+++ graphFromFile.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func graphFromFile(pathToFile string) [][]uint {
+	file, _ := os.Open(pathToFile)
+	defer file.Close()
+	vertice := 0
+	adjacencyList := make([][]uint, 0)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		adjacencyList = append(adjacencyList, make([]uint, 0))
+		line := scanner.Text()
+		relations := strings.Fields(line)
+		for i, v := range relations {
+			number, _ := strconv.Atoi(v)
+			if number == 1 {
+				adjacencyList[vertice] = append(adjacencyList[vertice], uint(i))
+			}
+		}
+		vertice++
+	}
+	return adjacencyList
+}
