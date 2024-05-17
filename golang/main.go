fecd608c49a324e5dda8837cf53d77cde230be4f
Maxim Tsvetkov <pushiniiist@gmail.com>
2024-05-17 05:09:28 +0300

    change matrix & make some formatting for output

--- golang/main.go
+++ golang/main.go
@@ -6,10 +6,10 @@ import (
 
 func main() {
 	graph := graphFromFile("adjacency_matrix")
-	fmt.Println(graph)
-	fmt.Println(bfs(graph, 0))
-	fmt.Println(bfsGetComps(graph))
-	fmt.Println(dfs(graph, 0))
-	fmt.Println(dfsGetComps(graph))
+	fmt.Println("Graph: ", graph)
+	fmt.Println("BFS: ", bfs(graph, 0))
+	fmt.Println("Find components via BFS: ", bfsGetComps(graph))
+	fmt.Println("DFS: ", dfs(graph, 0))
+	fmt.Println("Find strong components via DFS: ", dfsGetComps(graph))
 	fmt.Scanf("h")
 }
